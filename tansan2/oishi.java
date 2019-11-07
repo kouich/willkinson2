9d564faf752436247aa959de0753a407742df13a
shishunki <15129@hakodate.kosen-ac.jp>
2019-11-07 10:26:38 +0900

    na

--- tansan2/oishi.java
+++ tansan2/oishi.java
@@ -6,14 +6,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
+
 public class oishi extends Actor
 {
+  
     /**
      * Act - do whatever the oishi wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
+        
         if( Greenfoot.isKeyDown( "right" ) ){
         setRotation(0);
         move(1);
