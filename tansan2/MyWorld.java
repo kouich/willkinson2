0ed2673f33434e3978f4e4470baf5154cf2522d1
Kureon1234 <15125@hakodate.kosen-ac.jp>
2019-12-12 09:48:06 +0900

    加

--- tansan2/MyWorld.java
+++ tansan2/MyWorld.java
@@ -18,7 +18,7 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         addObject( new oishi(), 300, 200 );
-        //int score = 100;
-        //showText('score',300,10);
+       //score = 100;
+        //showText(score,300,10);
     }
 }
