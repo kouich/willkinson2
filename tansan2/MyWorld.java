06364666fe165b796db9049592572f29809bd063
shishunki <15129@hakodate.kosen-ac.jp>
2019-12-12 10:24:35 +0900

    c

--- tansan2/MyWorld.java
+++ tansan2/MyWorld.java
@@ -9,6 +9,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyWorld extends World
 {
     int count=0;
+   
+  
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -17,18 +19,19 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        addObject( new oishi(), 300, 200 );
-
-       
+            addObject( new oishi(), 300, 200 );
            addObject( new esa(), 550, 200 );
            addObject( new esa(), 50, 200 );
            addObject( new esa(), 330, 550 );
            addObject( new esa(), 300, 25 );
            addObject( new esa(), 200, 100 );
 
-        //int score = 100;
-        //showText('score',300,10);
+        int score = 100;
+        showText("score  "+ String.valueOf(score),400,10);
 
     }
     
 }
+
+
+
