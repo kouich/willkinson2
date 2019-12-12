d20a6080fb8120aabe706b8a7cc974e74cf607f8
shishunki <15129@hakodate.kosen-ac.jp>
2019-12-12 09:43:46 +0900

    a

---
+++ tansan2/esa.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class esa here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class esa extends Actor
+{
+    /**
+     * Act - do whatever the esa wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        Actor actor = getOneObjectAtOffset( 0, 0, oishi.class );
+        if( actor != null ){
+          getWorld().removeObject( this ); // getWorld() は World オブジェクトの getter
+        } 
+    }    
+}
